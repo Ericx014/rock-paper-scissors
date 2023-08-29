6bc1a6f013fe550297cdb1bf2530e7b2b155e0a2
Eric Bong <ericbong6868@gmail.com>
2023-08-29 18:07:41 +0800

    Created play function

--- javascript.js
+++ javascript.js
@@ -1,8 +1,26 @@
 const choices = ['Rock','Paper','Scissors'];
+let message;
+let computerChoice;
+let playerChoice;
 
 function getComputerChoice(){
     const randomIndex = Math.floor(Math.random() * choices.length);
     return choices[randomIndex];
 }
 
-console.log(getComputerChoice())
+function play(playerChoice, computerChoice){
+    playerChoice = playerChoice.toLower();
+
+    if (computerChoice == 'rock' && playerChoice == 'scissors'){
+        message = "You Lose! Rock beats Scissors";
+    } 
+    else if (computerChoice == 'paper' && playerChoice == 'rock'){
+        message = "You Lose! Paper beats Rock";
+    }
+    else if (computerChoice == 'scissors' && playerChoice == 'paper'){
+        message = "You Lose! Scissors beat Paper";
+    }
+}
+
+computerChoice = getComputerChoice();
+console.log(computerChoice);
